b43f6c72fb3070bc60145d16c20229137ae8cbe4
ColdDionysus <gautampukar02@gmail.com>
2023-08-16 17:36:31 +0545

    Latest Commit

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require("express");
 
 const app = express();
 
+app.get("/:firstName/:lastName/:id", (req, res) => {
+  const { firstName, lastName, id } = req.params;
+  res.send(`I am ${firstName} ${lastName}, ID: ${id}`);
+});
+
+app.post("/", (req, res) => {});
+
+app.put("/", (req, res) => {});
+
+app.delete("/", (req, res) => {});
+
 const PORT = process.env.PORT || 3500;
-
-app.get("/", () => {});
-
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
