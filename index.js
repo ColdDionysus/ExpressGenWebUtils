d0ce3bdad8cc16d3a68f44844a377acee8ce957e
ColdDionysus <gautampukar02@gmail.com>
2023-08-18 17:43:31 +0545

    TRY CATCH BODy

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const indexRouter = require("./routes/index");
 const app = express();
-
+app.use(express.json());
 app.use("/", indexRouter);
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3600;
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -4,34 +4,50 @@ const currencyConverter = require("../modules/currency");
 const pdfkit = require("../modules/pdfkit");
 const fs = require("fs");
 
-router.get("/qr", async (req, res) => {
-  const { qr } = req.query;
-  const qrData = await QRCode.toDataURL(qr);
-  res.send(`<img src="${qrData}"></img>`);
+router.post("/qr", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { name: qr } = req.body;
+    const qrData = await QRCode.toDataURL(qr);
+    res.send(`<img src="${qrData}"></img>`);
+  } catch (err) {
+    console.log(err);
+    res.send("Something went wrong");
+  }
 });
 
 router.get("/currency", async (req, res) => {
-  const toConvert = new currencyConverter();
-  console.log(req.query);
-  const { from, to, amount } = req.query;
+  try {
+    const toConvert = new currencyConverter();
+    console.log(req.query);
+    const { from, to, amount } = req.query;
 
-  const converter = await toConvert
-    .from(from)
-    .to(to)
-    .amount(parseInt(amount))
-    .convert();
+    const converter = await toConvert
+      .from(from)
+      .to(to)
+      .amount(parseInt(amount))
+      .convert();
 
-  res.send(`<h1>The currency converted is ${converter}</h1>`);
+    res.send(
+      `<h1>Currency from ${from} to ${to} for today rate is:  ${converter}</h1>`
+    );
+  } catch (err) {
+    res.send("Something went wrong");
+  }
 });
 
 router.get("/generate-pdf", (req, res) => {
-  const document = new pdfkit();
-  document.pipe(fs.createWriteStream("output.pdf"));
-  const { q } = req.query;
-  const addText = document.text(q);
-  document.end();
+  try {
+    const document = new pdfkit();
+    document.pipe(fs.createWriteStream("output.pdf"));
+    const { q } = req.query;
+    const addText = document.text(q);
+    document.end();
 
-  res.download("output.pdf");
+    res.download("output.pdf");
+  } catch (err) {
+    res.send("Something went wrong");
+  }
 });
 
 module.exports = router;
